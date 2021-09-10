086a21f4a273d97f577a36b5052af4000ead9255
Nour A Alghamdi <65490460+noor-alghamdi@users.noreply.github.com>
2021-09-10 20:46:20 +0000

    initiate the game screen

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+# import pygame;
+
+# def main():
+#   width = 500
+#   hieght = 500
+
+#   window = pygame.display.set_mod((width, hieght))
+#   snake = snake( (0,0,0),  (10, 10) )
+import pygame
+pygame.init()
+dis=pygame.display.set_mode((400,300))
+pygame.display.update()
+pygame.display.set_caption('Snake game by noor')
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)   #prints out all the actions that take place on the screen
  
+pygame.quit()
+quit()
