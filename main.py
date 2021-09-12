100bb6d1bd7f53f942ee426e5449132f1d67b0b1
Nour A Alghamdi <65490460+noor-alghamdi@users.noreply.github.com>
2021-09-12 15:38:44 +0000

    build the # game logic

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ curses.noecho()
 curses.curs_set(0)
 window.border(0)
 window.nodelay(1)
+
+# game logic 
+score = 0
+
+while True:
+    event = window.getch() # waiting for the next user input (getch())means get the next character
+     
+
+    # ...
+curses.endwin()
+print("Final score is = " + score)
