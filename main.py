2e32f116dd2a7bd74d39218b1d5728209cbf002e
Nour A Alghamdi <65490460+noor-alghamdi@users.noreply.github.com>
2021-09-12 15:38:20 +0000

    setup the window

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-# import pygame;
+import curses
 
-# def main():
-#   width = 500
-#   hieght = 500
-
-#   window = pygame.display.set_mod((width, hieght))
-#   snake = snake( (0,0,0),  (10, 10) )
-import pygame
-pygame.init()
-dis=pygame.display.set_mode((400,300))
-pygame.display.update()
-pygame.display.set_caption('Snake game by noor')
-game_over=False
-while not game_over:
-    for event in pygame.event.get():
-        print(event)   #prints out all the actions that take place on the screen
- 
-pygame.quit()
-quit()
+# setup window
+curses.initscr()
+window = curses.newwin(20, 60, 0, 0)
+window.keypad(1)
+curses.noecho()
+curses.curs_set(0)
+window.border(0)
+window.nodelay(1)
