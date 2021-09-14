168b7f630df83b955ea5b292078a4734a91d1252
Nour A Alghamdi <65490460+noor-alghamdi@users.noreply.github.com>
2021-09-14 10:26:12 +0000

    add snake and food objects

--- main.py
+++ main.py
@@ -9,13 +9,18 @@ curses.curs_set(0)
 window.border(0)
 window.nodelay(1)
 
+#food and snake
+snake = [(4,10), (4, 9 ), (4, 8)]
+food = (10, 20)
+
 # game logic 
 score = 0
 
 while True:
     event = window.getch() # waiting for the next user input (getch())means get the next character
-     
-
-    # ...
+    for c in snake:
+      window.addch(c[0], c[1], "-")
+    
+    window.addch(food[0], food[1], "*")
 curses.endwin()
 print("Final score is = " + score)
